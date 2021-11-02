0366bca07b4d7da9fc80779d43a2ce04af002113
jpfalc <31944810+jpfalc@users.noreply.github.com>
2021-11-01 21:48:26 -0400

    updated calcs to numpy for speed, easier centering/zooming of image

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,22 +3,6 @@ import numpy as np
 from datetime import datetime
 from datetime import timedelta
 
-# faster divergence check; precalculates real^2 and imaginary^2, avoids generic multiplication
-def check_diverge(z, c, max_iterations=256):
-    a = z[0]
-    b = z[1]
-    a2 = a*a
-    b2 = b*b
-    for iteration in range(max_iterations):
-        a_next = a2 - b2 + c[0]
-        b_next = 2*a*b + c[1]
-        a,b = a_next, b_next
-        a2 = a*a
-        b2 = b*b
-        if a2 + b2 > 4:
-            return(iteration)
-    return(iteration)
-
 # calculates pixel intensity based on # of steps needed to diverge
 def pixel_intensity(i, n, exponent, low, high):
     percentile = (i / (n-1)) ** exponent
@@ -38,51 +22,78 @@ def make_palette(n, low=(0,0,0), high=(255,255,255), exponents=(1,1,1), interior
         palette[n-1] = interior
     return palette
 
-# create mandlebrot image
-def make_image(palette, filename, z_initial=(0,0), dimensions=(318,300), left=-2, right=.65, top=1.25, bottom=-1.25, max_iterations=256):
-    width, height = dimensions
-    x_scale = (right - left) / width
-    y_scale = (top - bottom) / height
-    pixels = []
-    start_time = datetime.now()
+# makes a numpy array containing the real-values for a particular row of pixels
+def make_array_reals(params):
+    row = []
+    for x in range(params['width']):
+        real_val = params['left'] + params['x_scale'] * (x+0.5)
+        row.append(real_val)
+    array = np.array(row, dtype=np.double)
+    return(array)
+
+def get_row_intensities(row_num, params, const_r):
+    a = np.full(params['width'], params['z_real'])
+    b = np.full(params['width'], params['z_imag'])
+    a2, b2 = np.square(a), np.square(b)
+    const_i = np.full(params['width'], params['bottom'] + params['y_scale'] * (row_num+0.5))
+    escape_times = np.full(params['width'], 0)
     
-    for y in range(height):
-        y_val = bottom + y_scale * (y+0.5)
-        row = []
-        for x in range(width):
-            x_val = left + x_scale * (x+0.5)
-            c = (x_val, y_val)
-            n_iter = check_diverge(z_initial, c, max_iterations)
-            row.append(palette[n_iter])
-        pixels.append(row)
+    for iteration in range(params['max_iterations']):
+        a_next = a2 - b2 + const_r
+        b_next = 2 * a * b + const_i
+        a, b = a_next, b_next
+        a2, b2 = np.square(a), np.square(b)
+        escape_times += a2 + b2 < 4
+    escape_times = np.minimum(escape_times, params['max_iterations']-1)
+    return(escape_times)
+
+def apply_palette(intensities, palette):
+    pixels = []
+    for i in range(len(intensities)):
+        pixels.append(palette[intensities[i]])
+    return(pixels)
+
+# zoom 1 means the lowest dimension has a range of 4. range is halved for each zoom level
+def make_params(center=(-0.675,0), zoom=1, width=1000, height=1000):
+    width_scale = width / min(width, height)
+    height_scale = height / min(width, height)
+    params = {}
+    params['z_real'] = 0
+    params['z_imag'] = 0
+    params['width'] = width
+    params['height'] = height
+    params['bottom'] = center[1] - 2 * height_scale * (2**-(zoom-1))
+    params['top'] = center[1] + 2 * height_scale * (2**-(zoom-1))
+    params['left'] = center[0] - 2 * width_scale * (2**-(zoom-1))
+    params['right'] = center[0] + 2 * width_scale * (2**-(zoom-1))
+    params['max_iterations'] = 256
+    params['x_scale'] = (params['right'] - params['left']) / params['width']
+    params['y_scale'] = (params['top'] - params['bottom']) / params['height']
+    return(params)
+
+
+def make_image(palette, filename, center=(-0.675,0), zoom=1, width=1200, height=1200):
+    start_time = datetime.now()
+    params = make_params(center, zoom, width, height)
+    reals = make_array_reals(params)
+    pixels = []
+    for row in range(params['height']):
+        row_intensities = get_row_intensities(row, params, reals)
+        row_pixels = apply_palette(row_intensities, palette)
+        pixels.append(row_pixels)
+
     array = np.array(pixels, dtype=np.uint8)
     img = Image.fromarray(array)
     img.save(filename)
     dt = datetime.now() - start_time
     ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
     print('runtime was ', ms, 'ms')
-    return pixels
+    return(pixels)
 
-palette_v1 = make_palette(n=256, high=(255,0,0))
-palette_v2 = make_palette(n=256, low=(255,0,0), high=(0,0,0))
-palette_v3 = make_palette(n=256, high=(128,128,255), interior=(0,0,0))
-palette_v4 = make_palette(n=256, high=(128,128,255), exponents=(.5,.5,.5), interior=(0,0,0))
-palette_v5 = make_palette(n=256, high=(255,128,255), exponents=(.75,2,.5), interior=(0,0,0))
+palette = make_palette(n=256, high=(255,128,255), exponents=(.75,2,.5), interior=(0,0,0))
 
-dim_small = (318,300)
-dim_large = (1272, 1200)
-dim_huge = (1272*2, 1200*2)
+# default image showing the whole set
+make_image(palette, 'img/mandlebrot.png', zoom=1.66, width=318*4, height=300*4)
 
-# test images
-#make_image(palette_v1, 'sm_mbrot_v1.png', dimensions=dim_small)
-#make_image(palette_v2, 'sm_mbrot_v2.png', dimensions=dim_small)
-#make_image(palette_v3, 'sm_mbrot_v3.png', dimensions=dim_small)
-#make_image(palette_v4, 'sm_mbrot_v4.png', dimensions=dim_small)
-make_image(palette_v5, 'sm_mbrot_v5_test2.png', dimensions=dim_small)
-        
-# main images. each of these takes ~10min to run
-#make_image(palette_v1, 'mbrot_v1.png')
-#make_image(palette_v2, 'mbrot_v2.png')
-#make_image(palette_v3, 'mbrot_v3.png')
-#make_image(palette_v4, 'mbrot_v4.png')
-#make_image(palette_v5, 'mbrot_v5.png')
+# zoomed in on a spiral
+make_image(palette, 'img/spiral.png', center=(-.66468458, .355508837), zoom=12)
